602b31234e54dd71160bf7bd6b15d77746cd7902
bryan lopez <blopez4434@gmail.com>
2024-05-21 16:14:42 -0400

    Update @nestjs/microservices to version 10.3.8

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(envs.port);
+  const logger = new Logger('OrdersMS-Main');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
+  await app.listen();
+  logger.log(`Orders Microservice is running on: ${envs.port}`);
 }
 bootstrap();
